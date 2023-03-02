ddc5306506d82a07357b1cf5b23568a4ddb150cb
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-03-02 22:07:16 +0530

    loading image and feature

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const loadData=()=>{
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
+    .then(res =>res.json())
+    .then(data=>displayData(data.data.tools))
+
+    
+}
+
+const displayData=(data)=>{
+    data.forEach(elements => {
+        // console.log(elements);
+   
+    const content=document.getElementById('content')
+    const text=document.createElement('div')
+    text.innerHTML=`
+    <div class="card" style="width: 18rem;">
+    <img src="${elements.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Features</h5>
+      <p class="card-text ">1.${elements.features[0]}</p>
+      <p class="card-text ">2.${elements.features[1]}</p>
+      <p class="card-text ">3.${elements.features[2]}</p>
+    </div>
+  </div>
+
+    `
+    content.appendChild(text)
+
+
+
+
+
+ 
+    
+  });
+
+}
+loadData()
