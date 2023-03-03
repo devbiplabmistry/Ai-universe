1134c7fe48233fd232e6864c9995c258e1377eb3
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-03-03 09:33:19 +0530

    creating modals

--- js/script.js
+++ js/script.js
@@ -17,8 +17,18 @@ const toggleSpinner =(isLoading)=>{
 }
 
 const displayData=(data)=>{
+  // console.log(data);
+  const show=document.getElementById('show');
+  if( data.length >6){
+    show.classList.remove('d-none')
+    data=data.splice(0,6)
+  }
+  else{
+    show.classList.add('d-none')
+  }
     data.forEach(elements => {
-        // console.log(elements)
+
+        // console.log(elements.id)
     const content=document.getElementById('content')
     const text=document.createElement('div')
     text.innerHTML=`
@@ -29,7 +39,7 @@ const displayData=(data)=>{
       <p class="card-text ">1.${elements.features[0]}</p>
       <p class="card-text ">2.${elements.features[1]}</p>
       <p class="card-text ">3.${elements.features[2]}</p>
-      <h4>${elements.name}   <a><i class="fa-solid fa-arrow-right ms-5"></i></a></h4>
+      <h4>${elements.name} <button onclick=detailsLoading(${elements.id})  type="button"  class="ms-5 border-0 rounded"   data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right"></i></button></h4>
       <p>${elements.published_in}</p>
     </div>
   </div>
@@ -45,10 +55,49 @@ toggleSpinner(false)
 
 
 document.getElementById('show').addEventListener('click',function(){
+  loadData()
 
 
 })
 
+
+const detailsLoading=(id)=>{
+fetch(`https://openapi.programming-hero.com/api/ai/tool/0${id}`)
+.then(res=>res.json())
+.then(data=>displayModal(data))
+
+}
+const displayModal=(data)=>{
+  console.log(data.data.features);
+  const title=document.getElementById('exampleModalLabel')
+  title.innerText=`${data.data.description}`
+  const basic=document.getElementById('basic')
+  basic.innerText=`${data.data.pricing[0].price}
+         ${data.data.pricing[0].plan}
+  `
+  const pro=document.getElementById('pro')
+  pro.innerText=`${data.data.pricing[1].price}
+         ${data.data.pricing[1].plan}
+  `
+  const Enterprise=document.getElementById('Enterprise')
+  Enterprise.innerText=`${data.data.pricing[2].price}
+         ${data.data.pricing[2].plan}
+  `
+
+  const featur=document.getElementById('featur')
+    const featurText=document.createElement('div')
+    featurText.innerHTML=`
+      <h5>Features</h5>
+      <p class="card-text ">1.${data.data.features[0].feature_name}</p>
+      <p class="card-text ">2.${elements.features[1].feature_name}</p>
+      <p class="card-text ">3.${elements.features[2].feature_name}</p>
+   
+    
+    `
+    featur.appendChild(featurText)
+
+
+}
 loadData()
 
 
