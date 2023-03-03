a6019145b0c03447d34e4e2bf7ee168bc2c3ede3
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-03-03 13:01:15 +0530

    error fixing

--- js/script.js
+++ js/script.js
@@ -5,13 +5,6 @@ const loadData=()=>{
     .then(data=>displayData(data.data.tools))
    
 }
-const loadingData2=()=>{ 
-  toggleSpinner(true)
-  fetch('https://openapi.programming-hero.com/api/ai/tools')
-  .then(res =>res.json())
-  .then(data=>displayData2(data.data.tools))
- 
-}
 
 const toggleSpinner =(isLoading)=>{
     const spin=document.getElementById('spinner')
@@ -26,13 +19,6 @@ const toggleSpinner =(isLoading)=>{
 const displayData=(data)=>{
   // console.log(data);
   const show=document.getElementById('show');
-  if( data.length >6){
-    show.classList.remove('d-none')
-    data=data.splice(0,6)
-  }
-  else{
-    show.classList.add('d-none')
-  }
     data.forEach(elements => {
 
         // console.log(elements.id)
@@ -43,9 +29,9 @@ const displayData=(data)=>{
     <img src="${elements.image}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Features</h5>
-      <p class="card-text ">1.${elements.features[0]}</p>
-      <p class="card-text ">2.${elements.features[1]}</p>
-      <p class="card-text ">3.${elements.features[2]}</p>
+      <p class="card-text ">1.${elements.features[0] ? elements.features[0]:'No data found' }</p>
+      <p class="card-text ">2.${elements.features[1] ? elements.features[1] :'No data found'}</p>
+      <p class="card-text ">3.${elements.features[2] ? elements.features[2] :'No data found'}</p>
       <h4>${elements.name} <button onclick=detailsLoading(${elements.id})  type="button"  class="ms-5 border-0 rounded"   data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right"></i></button></h4>
       <p>${elements.published_in}</p>
     </div>
@@ -57,98 +43,45 @@ const displayData=(data)=>{
 toggleSpinner(false)
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const displayData2=(data)=>{
-  // console.log(data);
-  data=data.splice(6,12)
-  const show=document.getElementById('show');
-    data.forEach(elements => {
-
-        // console.log(elements.id)
-    const content=document.getElementById('content')
-    const text=document.createElement('div')
-    text.innerHTML=`
-    <div class="card" style="width: 18rem;">
-    <img src="${elements.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Features</h5>
-      <p class="card-text ">1.${elements.features[0]}</p>
-      <p class="card-text ">2.${elements.features[1]}</p>
-      <p class="card-text ">3.${elements.features[2]}</p>
-      <h4>${elements.name} <button onclick=detailsLoading(${elements.id})  type="button"  class="ms-5 border-0 rounded"   data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right"></i></button></h4>
-      <p>${elements.published_in}</p>
-    </div>
-  </div>
-    `
-    content.appendChild(text)
-  });
-//   stop spinner
-toggleSpinner(false)
-
-}
-
-document.getElementById('show').addEventListener('click',function(){
-  loadingData2()
-  const show=document.getElementById('show');
-  show.classList.add('d-none')
-})
-
 
 const detailsLoading=(id)=>{
-fetch(`https://openapi.programming-hero.com/api/ai/tool/0${id}`)
+  let i='0'+id
+  if(i.length==2){
+    i='0'+id
+  }
+  else{
+    i=id;
+  }
+ 
+fetch(`https://openapi.programming-hero.com/api/ai/tool/${i}`)
 .then(res=>res.json())
 .then(data=>displayModal(data))
 
 }
 const displayModal=(data)=>{
-  console.log(data.data.input_output_examples[0]
-
-    )
   const title=document.getElementById('exampleModalLabel')
-  title.innerText=`${data.data.description}`
+  title.innerText=`${data.data.description ? data.data.description :'No data ' }`
   const basic=document.getElementById('basic')
-  basic.innerText=`${data.data.pricing[0].price}
-         ${data.data.pricing[0].plan}
+  basic.innerText=`${data.data.pricing[0].price ? data.data.pricing[0].price : 'No data found'}
+         ${data.data.pricing[0].plan ? data.data.pricing[0].plan :'No data found'}
   `
   const pro=document.getElementById('pro')
-  pro.innerText=`${data.data.pricing[1].price}
-         ${data.data.pricing[1].plan}
+  pro.innerText=`${data.data.pricing[1].price ? data.data.pricing[1].price :'no data found' }
+         ${data.data.pricing[1].plan ? data.data.pricing[1].plan  :'no data found' }
   `
   const Enterprise=document.getElementById('Enterprise')
-  Enterprise.innerText=`${data.data.pricing[2].price}
-         ${data.data.pricing[2].plan}
+  Enterprise.innerText=`${data.data.pricing[2].price ? data.data.pricing[2].price :'no data found'}
+         ${data.data.pricing[2].plan ? data.data.pricing[2].plan  :'no data found' }
   `
-
+ 
   const featur=document.getElementById('featur')
   featur.innerText=''
     const featurText=document.createElement('div')
     featurText.innerHTML=`
       <h5>Features</h5>
-      <p class="card-text ">1.${data.data.features['1'].feature_name}</p>
-      <p class="card-text ">2.${data.data.features['2'].feature_name}</p>
-      <p class="card-text ">3.${data.data.features['3'].feature_name}</p>
+      <p class="card-text ">1.${data.data.features['1'].feature_name ? data.data.features['1'].feature_name :'No data found'}</p>
+      <p class="card-text ">2.${data.data.features['2'].feature_name ?data.data.features['2'].feature_name:'No data found' }</p>
+      <p class="card-text ">3.${data.data.features['3'].feature_name ? data.data.features['3'].feature_name :'No data found'}</p>
     `
     featur.appendChild(featurText)
 
@@ -157,9 +90,9 @@ const displayModal=(data)=>{
     const integrationText=document.createElement('div')
     integrationText.innerHTML=`
       <h5>Integrations</h5>
-      <p class="card-text ">1.${data.data.integrations[0]}</p>
-      <p class="card-text ">2.${data.data.integrations[1]}</p>
-      <p class="card-text ">3.${data.data.integrations[2]}</p>
+      <p class="card-text ">1.${data.data.integrations[0] ? data.data.integrations[0] :'No data found'}</p>
+      <p class="card-text ">2.${data.data.integrations[1] ? data.data.integrations[1] :'No data found'}</p>
+      <p class="card-text ">3.${data.data.integrations[2] ?data.data.integrations[2] :'No data found'}</p>
     `
     integration.appendChild(integrationText)
 
@@ -173,8 +106,8 @@ const displayModal=(data)=>{
     input.innerText=''
     const div=document.createElement('div')
     div.innerHTML=`
-    <h3>${data.data.input_output_examples[0].input}</h3>
-    <p class="mt-4" >${data.data.input_output_examples[0].output}</p>  
+    <h3>${data.data.input_output_examples[0].input ? data.data.input_output_examples[0].input :'No data found' }</h3>
+    <p class="mt-4" >${data.data.input_output_examples[0].output ? data.data.input_output_examples[0].output : 'No data found'}</p>  
     `
 input.appendChild(div)
 
